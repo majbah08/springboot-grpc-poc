fb25d6a474e2bc0164b5f1b695ab1975a3ec5113
majbah08 <majbahbuet08@gmail.com>
2023-12-26 07:55:04 +0600

    initial commit

---
+++ src/main/java/com/majbah/springgrpcpoc/SpringGrpcPocApplication.java
@@ -0,0 +1,13 @@
+package com.majbah.springgrpcpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGrpcPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGrpcPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/majbah/springgrpcpoc/SpringGrpcPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.majbah.springgrpcpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGrpcPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
